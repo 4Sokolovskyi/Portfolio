a6d31cc9d411b31295d026cf6450fe0bdb2261f6
Pavlo Sokolovskyi <mainfhr@gmail.com>
2024-04-11 19:12:07 +0200

    details and setings

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
    return (
       <div className='footer'>
          <a href="https://linkedin.com/in/pavlo-sokolovskyi-078958280" target="_blanc"><FaLinkedin/></a>
-         <a href="https://github.com/4egoPoint" target="_blanc"><FaGithub/></a>
+         <a href="https://github.com/4Sokolovskyi" target="_blanc"><FaGithub/></a>
          <a href="https://www.instagram.com/q3.wwe._" target="_blanc"><FaInstagram/></a>
       </div>
    )
